28ce472c0bca37cac32eeaff30ec7b3a38af6a83
Korolev Kirill <antoninakoroleva16@gmail.com>
2023-01-24 00:28:04 +0700

    исправлен код стайл

--- src/server.h
+++ src/server.h
@@ -3,16 +3,16 @@
 
 #define MAX_PLAYER_COUNT 7
 
-struct Player
+struct player
 {
     const char *name;
     unsigned int shapeType;
     int socket;
 };
 
-Player players[MAX_PLAYER_COUNT];
+player players[MAX_PLAYER_COUNT];
 
-static unsigned int playersConnected = 0;
-static unsigned int currentPlayer = 0;
+static unsigned int players_connected = 0;
+static unsigned int current_player = 0;
 
 #endif //SERVER_H
