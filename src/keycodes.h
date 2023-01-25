90476f4a54ac40bf24368a27c46980d73760b8d2
Vladislav <vlad.zykov.1993@gmail.com>
2023-01-26 00:17:47 +0700

    Fixed include guards (refs#483)

--- src/keycodes.h
+++ src/keycodes.h
@@ -1,4 +1,9 @@
+#ifndef KEYCODES_H
+#define KEYCODES_H
+
 #define KEY_ARROW_UP        1
 #define KEY_ARROW_DOWN      2      
 #define KEY_ARROW_LEFT      3
 #define KEY_ARROW_RIGHT     4
+
+#endif //   KEYCODES_H
