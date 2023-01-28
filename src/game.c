091ec9c92f47f296f034e7b118ffc04f963de951
aleksandr.akinshin <Ulikik@mail.ru>
2023-01-29 05:14:46 +0700

    добавлен функционал столкновения с кучей, сгорания заполненных линий, появления и падения блока issue_#450
    Исправлено повторение объявления функции issue_#450

--- src/game.c
+++ src/game.c
@@ -8,6 +8,9 @@ int position_x            = 0;
 int position_y            = 0;
 unsigned int number_block = 0;
 
+unsigned int *players_connected_pointer = NULL;
+int players_shapes[7] = {0};
+
 struct tetris_block
 {
     char data[5][5];
@@ -129,4 +132,121 @@ int process_cup_border_collisions(void)
     }
 
     return 0;
+}
+
+int process_heap_collisions(void)
+{ // Проверка на столкновение: проход по высоте
+    for (int l = 0; l < current_block.height; ++l)
+    {
+        // проход по ширине
+        for (int i = position_x + current_block.width - 1; i >= position_x; --i)
+        {
+            // Условие столкновения фигуры с дном или с кучей
+            if ((cup[position_y + current_block.height - l][i] != ' ') &&
+                (current_block.data[current_block.height - 1 - l][i - position_x] != ' '))
+            {
+                // функция добавления фигуры в матрицу стакана
+                add_shape_in_cup(cup);
+                total_score += 5;
+                // Проверка на заполнение линий
+                line_is_full();
+
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+void add_shape_in_cup(char cup_buf[CUPSIZE_Y][CUPSIZE_X])
+{
+    for (int i = 0; i < current_block.height; ++i)
+    {
+        for (int j = 0; j < current_block.width; ++j)
+        {
+            if (current_block.data[i][j] != ' ')
+            {
+                cup_buf[position_y + i][position_x + j] = current_block.data[i][j];
+            }
+        }
+    }
+}
+
+void line_is_full(void)
+{
+    int counter = 0;
+
+    for (int i = CUPSIZE_Y - 2; i > 0; --i)
+    {
+        for (int j = 1; j < CUPSIZE_X - 1; ++j)
+        {
+            if (cup[i][j] != ' ')
+            {
+                ++counter;
+            }
+        }
+
+        if (counter == CUPSIZE_X - 2)
+        {
+            clear_line_from_cup(i);
+            i = CUPSIZE_Y - 1;
+        }
+
+        counter = 0;
+    }
+}
+
+void clear_line_from_cup(int number)
+{
+    int counter = 0;
+
+    for (int i = number; i > 1; --i)
+    {
+        for (int j = 1; j < CUPSIZE_X - 1; ++j)
+        {
+            cup[i][j] = cup[i - 1][j];
+
+            if (cup[i][j] == ' ')
+            {
+                ++counter;
+            }
+        }
+
+        if (counter == CUPSIZE_X - 2)
+        {
+            total_score += 50;
+            return;
+        }
+
+        counter = 0;
+    }
+}
+
+void spawn_new_block(void)
+{
+    number_block = players_shapes[generate_random_number(0, *players_connected_pointer - 1)];
+    current_block = blocks[number_block];
+
+    position_x = (CUPSIZE_X / 2) - (current_block.width / 2);
+    position_y = 0;
+
+    if (process_heap_collisions())
+    {
+        game_over = 1;
+    }
+
+    if (total_score > game_level * 500 && game_level < 8)
+    {
+        game_level++;
+    }
+}
+
+void block_drop(void)
+{
+    if (process_heap_collisions())
+    {
+        spawn_new_block();
+    }
+
+    position_y++;
 }
--- src/game.h
+++ src/game.h
@@ -10,11 +10,20 @@
 #include <sys/types.h>
 #include <termios.h>
 #include <fcntl.h>
+#include "util.h"
 
 #define CUPSIZE_X 12
 #define CUPSIZE_Y 21
 
 void process_rotations(void);
+
 int process_cup_border_collisions(void);
+
+int process_heap_collisions(void);
+void add_shape_in_cup(char cup_buf[CUPSIZE_Y][CUPSIZE_X]);
+void line_is_full(void);
+void clear_line_from_cup(int number);
+void block_drop(void);
+void spawn_new_block(void);
 
 #endif //GAME_H
